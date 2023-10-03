43954956a1f1c6a5201ca6f6764b44160a451140
jyotirmoy1997 <subhodas45036@gmail.com>
2023-10-03 13:18:02 +0530

    Added YouTube logo in footer section, reversed footer section order, added padding top-bottom to gallery preview, fixed nav links spacing

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -5,12 +5,31 @@ import phoneLogo from "../../assets/logos/telephone.png"
 import mailLogo from "../../assets/logos/gmail.png"
 import facebookLogo from "../../assets/logos/facebook.png"
 import instagramLogo from "../../assets/logos/instagram.png"
+import youtubeLogo from "../../assets/logos/youtube.png"
 
 const Footer = () => {
     const d = new Date(Date.now())
     return(
         <div className="footer-wrapper">
             <div className="footer">
+                
+                <div className="footer-info">
+                    <h3>About D&D Productions</h3>
+                    <p>
+                        Follow Us
+                    </p>
+                    <div className="footer-icons">
+                        <a href="https://www.facebook.com/profile.php?id=100084598611956" target="_blank">
+                            <img src={facebookLogo} alt="" srcset="" height="30px" width="30px"/>
+                        </a>
+                        <a href="https://www.instagram.com/dandd.productions/" target="_blank">
+                            <img src={instagramLogo} alt="" srcset="" height="30px" width="30px"/>
+                        </a>
+                        <a href="https://www.youtube.com/@ddproductions664" target="_blank">
+                            <img src={youtubeLogo} alt="" srcset="" height="30px" width="30px"/>
+                        </a>
+                    </div>
+                </div>
                 <div className="footer-contact">
                     <div className="footer-contact-items">
                             <img src={locationLogo} alt="" srcset="" height="30px" width="30px"/>
@@ -26,20 +45,6 @@ const Footer = () => {
                             <img src={mailLogo} alt="" srcset="" height="30px" width="30px"/>
                             <p>dasandduttaproductions@gmail.com</p>
                             
-                    </div>
-                </div>
-                <div className="footer-info">
-                    <h3>About D&D Productions</h3>
-                    <p>
-                        About
-                    </p>
-                    <div className="footer-icons">
-                        <a href="https://www.facebook.com/profile.php?id=100084598611956" target="_blank">
-                            <img src={facebookLogo} alt="" srcset="" height="30px" width="30px"/>
-                        </a>
-                        <a href="https://www.instagram.com/dandd.productions/" target="_blank">
-                            <img src={instagramLogo} alt="" srcset="" height="30px" width="30px"/>
-                        </a>
                     </div>
                 </div>
             </div>
--- src/routes/gallery/gallery.component.jsx
+++ src/routes/gallery/gallery.component.jsx
@@ -1,24 +1,27 @@
 import GalleryPreview from "../../components/gallery-preview/gallery-preview.component"
 import { galleryPreviewImages } from "../../assets/imageLinks"
+import Footer from "../../components/footer/footer.component"
+import "./gallery.styles.css"
 
 
 const Gallery = () => {
     return(
-        <div>
-            {
-                galleryPreviewImages.map((content) => {
-                    return(
-                        <div key={content.id}>
-                            <GalleryPreview 
-                                logo={content.logo} 
-                                imageArray={content.images} 
-                            />
-                        </div>
-                    )
-                })
-            }
-
-            
+        <div className="galley-wrapper">
+            <div className="gallery-wrapper-inner">
+                {
+                    galleryPreviewImages.map((content) => {
+                        return(
+                            <div key={content.id}>
+                                <GalleryPreview 
+                                    logo={content.logo} 
+                                    imageArray={content.images} 
+                                />
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <Footer/>
         </div>
     )
 }
