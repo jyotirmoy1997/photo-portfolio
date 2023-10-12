106f3f1ef8b4167055228cdacf98a6feb12eb4a3
jyotirmoy1997 <subhodas45036@gmail.com>
2023-10-12 11:11:45 +0530

    Modified Image Page - Phase 1

--- src/components/images-page/images-page.component.jsx
+++ src/components/images-page/images-page.component.jsx
@@ -1,20 +1,27 @@
+import Footer from "../footer/footer.component"
 import "./images-page.styles.css"
+import img1 from "./img1.jpg"
+import img2 from "./img2.jpg"
+import img3 from "./img3.jpg"
+import img4 from "./img4.jpg"
 
 const ImagePage = () => {
     return(
-        <div className="image-page-wrapper">
-            <div className="image-desc">
-                Image Description
+        <>
+            <div className="image-page-wrapper">
+                <div className="image-desc">
+                    Image Description
+                </div>
+                <div className="gallery-image-slider">
+                    <div className="img-x"><img src={img1} alt="" srcset="" /></div>
+                    <div className="img-x"><img src={img2} alt="" srcset="" /></div>
+                    <div className="img-x"><img src={img3} alt="" srcset="" /></div>
+                    <div className="img-x"><img src={img4} alt="" srcset="" /></div>
+                </div>
             </div>
-            <div className="gallery-image-slider">
-                <div className="img-x">X</div>
-                <div className="img-x">X</div>
-                <div className="img-x">X</div>
-                <div className="img-x">X</div>
-                <div className="img-x">X</div>
-                <div className="img-x">X</div>
-            </div>
-        </div>
+            <Footer/>
+        </>
+        
     )
 }
 
